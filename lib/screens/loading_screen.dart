0e8c04e5169a2c8f6b54cde4520d6eca63a893ef
aymanyusif2121 <aymanyusif120@gmail.com>
2021-07-26 19:55:34 +0300

    updated

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:weather_clime/screens/get_started_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -27,13 +28,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SpinKitCircle(
-          size: 100.0,
-          color: Colors.yellow,
-        ),
-      ),
+    return Center(
+      child: FutureBuilder(builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text('Loading');
+        } else if (snapshot.hasError) {
+          return Text('Error : ${snapshot.error}  ');
+        } else {
+          return SizedBox(
+            width: 60,
+            height: 60,
+            child: CircularProgressIndicator(),
+          );
+        }
+      }),
     );
   }
 }
